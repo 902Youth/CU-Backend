0c6a8bd9045399b335de512e4db2caba4c1e03bc
Vador92 <119339398+Vador92@users.noreply.github.com>
2024-06-12 11:20:47 -0400

    fixed sqlalchemy for sign up

--- app/controllers/auth.py
+++ app/controllers/auth.py
@@ -1,4 +1,6 @@
 import datetime
+import binascii
+
 import uuid
 from flask import Flask, session, render_template, jsonify, request, make_response, redirect, url_for, flash, Blueprint, Response
 from flask_cors import CORS
@@ -20,11 +22,21 @@ from methods.jsonmessages import create_success_response, create_error_reponse
 #used to establish the current state of login
 from flask_login import login_user, login_required, logout_user, current_user
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+
 
 
 
 load_dotenv()
 auth = Blueprint('auth', __name__)
+
+DATABASE_URL = os.getenv('DATABASE_URL')
+
+engine = create_engine(DATABASE_URL, echo=False)
+Session = scoped_session(sessionmaker(bind=engine))
+
+
 
 
 #connect to database
@@ -38,8 +50,7 @@ db_config = {
 }
 
 def get_db_connection():
-    return mysql.connector.connect(**db_config) 
-
+    return Session()
 
 def generate_token(username):
     #avoid circular import
@@ -73,7 +84,7 @@ def generate_token(username):
 #     return decorated
 
 
-# # redirect login to home page
+# redirect login to home page
 # @auth.route('/login', methods=['POST'])
 # def login():
 #     if request.method == 'POST':
@@ -137,109 +148,120 @@ def sign_up():
 
 
         # establish connection to database
-        connection = get_db_connection()
+        session = get_db_connection()
 
         # get the cursor for the first search term
-        cursor = connection.cursor(dictionary=True)
-
-        # target the email address
-        cursor.execute("SELECT * FROM user WHERE email = %s", (email,))
-        existing_email = cursor.fetchone()
-
-        # target the username
-        cursor.execute("SELECT * FROM user WHERE username = %s", (username,))
-        existing_username = cursor.fetchone()
-
-        #target the mobile phone number
-        cursor.execute("SELECT * FROM user WHERE mobile = %s", (mobile,))
-        existing_mobile = cursor.fetchone()
-        
-
-         # Perform basic validation
-        if not email or not username or not password or not confirm_password:
-            return jsonify({'status': 'fail', 'message': 'Missing required fields'}), 400
-
-        if email != confirm_email:
-            return jsonify({'status': 'fail', 'message': 'Emails do not match'}), 400
-
-        if password != confirm_password:
-            return jsonify({'status': 'fail', 'message': 'Passwords do not match'}), 400
-        
-
-        upper_case, lower_case, special, digits, is_common= get_password_strength(password)
-        if is_common:
-            return jsonify({'status': 'fail', 'message': 'Password is too common, please try again.'}), 400
-            
-        if len(password) < 8:
-            return jsonify({'status': 'fail', 'message': 'Password must be atleast 8 characters long'}), 400
-
-        if (validate_email(email) == False):
-            return jsonify({'status': 'fail', 'message': 'Email is not valid'}), 400
-
-        if upper_case == 0:
-            return jsonify({'status': 'fail', 'message': 'Must include atleast one upper case character'}), 400
-        
-        if lower_case == 0:
-            return jsonify({'status': 'fail', 'message': 'Must include atleast one lower case character'}), 400
-        
-        if digits == 0:
-            return jsonify({'status': 'fail', 'message': 'Must include atleast one digit'}), 400
-        
-        if special == 0:
-            return jsonify({'status': 'fail', 'message': 'Must include atleast one special character eg. #'}), 400
-        
-        if mobile.isdigit() == False:
-            return jsonify({'status': 'fail', 'message': 'Phone number must be all numbers example: (1234567890)'}), 400
-        
-        if (existing_email):
-            return jsonify({'status': 'fail', 'message': 'User with email already exists'}), 400
-        if (existing_username):
-            return jsonify({'status': 'fail', 'message': 'Username already exists'}), 400
-        if (existing_mobile):
-            return jsonify({'status': 'fail', 'message': 'User with mobile number already exists'}), 400
-
-
-        hashed_password = generate_password_hash(password)
-        # # Assuming the user is successfully created and authenticated
-        new_user = User(fullname=fullname, mobile=mobile, username=username, email=email, hash=hashed_password)
-
-        # logic for inserting a new user into database
-
+        #cursor = session.cursor(dictionary=True)
         try: 
-            insert_query = "INSERT INTO `user` (id, username, mobile, email, hash, registeredAt, lastLogin, fullname) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
-            insert_data = (new_user.id, new_user.username, new_user.mobile, new_user.email, new_user.hash, new_user.registeredAt, new_user.lastLogin, new_user.fullname)
-            cursor.execute(insert_query, insert_data)
-            # Close cursor and connection
-            connection.commit()
+        # target the email address
+            existing_email = session.query(User).filter_by(email=email).first()
+            existing_username = session.query(User).filter_by(username=username).first()
+            existing_mobile = session.query(User).filter_by(mobile=mobile).first()
+
+
+            print(existing_email, existing_username, existing_mobile)
+
+
+            # cursor.execute("SELECT * FROM user WHERE email = %s", (email,))
+            # existing_email = cursor.fetchone()
+            # target the username
+            # cursor.execute("SELECT * FROM user WHERE username = %s", (username,))
+            # existing_username = cursor.fetchone()
+            # target the mobile phone number
+            # cursor.execute("SELECT * FROM user WHERE mobile = %s", (mobile,))
+            # existing_mobile = cursor.fetchone()
+    
+
+            # Perform basic validation
+            if not email or not username or not password or not confirm_password:
+                return jsonify({'status': 'fail', 'message': 'Missing required fields'}), 400
+
+            if email != confirm_email:
+                return jsonify({'status': 'fail', 'message': 'Emails do not match'}), 400
+
+            if password != confirm_password:
+                return jsonify({'status': 'fail', 'message': 'Passwords do not match'}), 400
+            
+
+            upper_case, lower_case, special, digits, is_common= get_password_strength(password)
+            if is_common:
+                return jsonify({'status': 'fail', 'message': 'Password is too common, please try again.'}), 400
+                
+            if len(password) < 8:
+                return jsonify({'status': 'fail', 'message': 'Password must be atleast 8 characters long'}), 400
+
+            if (validate_email(email) == False):
+                return jsonify({'status': 'fail', 'message': 'Email is not valid'}), 400
+
+            if upper_case == 0:
+                return jsonify({'status': 'fail', 'message': 'Must include atleast one upper case character'}), 400
+            
+            if lower_case == 0:
+                return jsonify({'status': 'fail', 'message': 'Must include atleast one lower case character'}), 400
+            
+            if digits == 0:
+                return jsonify({'status': 'fail', 'message': 'Must include atleast one digit'}), 400
+            
+            if special == 0:
+                return jsonify({'status': 'fail', 'message': 'Must include atleast one special character eg. #'}), 400
+            
+            if mobile.isdigit() == False:
+                return jsonify({'status': 'fail', 'message': 'Phone number must be all numbers example: (1234567890)'}), 400
+            
+            if len(mobile) != 10:
+                return jsonify({'status': 'fail', 'message': 'mobile must be 10 characters'})
+            
+            if (existing_email):
+                return jsonify({'status': 'fail', 'message': 'User with email already exists'}), 400
+            if (existing_username):
+                return jsonify({'status': 'fail', 'message': 'Username already exists'}), 400
+            if (existing_mobile):
+                return jsonify({'status': 'fail', 'message': 'User with mobile number already exists'}), 400
+
+
+            hashed_password = generate_password_hash(password)
+            hashed_password = hashed_password.encode('utf-8')
+
+            # Assuming the user is successfully created and authenticated
+            new_user = User(fullname=fullname, mobile=mobile, username=username, email=email, hash=hashed_password)
+
+            # # logic for inserting a new user into database
+
+        
+            session.add(new_user)
+            session.commit()
+
+            user_id = new_user.id
+            auth_token = generate_token(user_id)
+
+
+            response_data = {
+                'status': 'success',
+                'message': 'User successfully registered',
+                'user': {
+                    'id': new_user.id,
+                    'email': new_user.email,
+                    'username': new_user.username,
+                    'fullname': new_user.fullname,
+                    'mobile': new_user.mobile,
+                },
+                'auth_token': auth_token
+            }
+            status_code = 201
+            response = make_response(jsonify(response_data), status_code)
+            response.headers['Content-Type'] = 'application/json'
+
+            # login_user(new_user)
+
+            return response
+
         except Exception as e:
             return jsonify({'status': 'error', 'message': e}), 400
         
-        cursor.close()
-        connection.close()
+        # cursor.close()
+        # connection.close()
 
-        user_id = new_user.id
-        auth_token = generate_token(user_id)
-
-
-        response_data = {
-            'status': 'success',
-            'message': 'User successfully registered',
-            'user': {
-                'id': new_user.id,
-                'email': email,
-                'username': username,
-                'fullname': fullname,
-                'mobile': mobile
-            },
-            'auth_token': auth_token
-        }
-        status_code = 201
-        response = make_response(jsonify(response_data), status_code)
-        response.headers['Content-Type'] = 'application/json'
-
-        login_user(new_user)
-
-        return response
+        
 
         # check if user is currently in the database using a query search on the email
         # insert block here
